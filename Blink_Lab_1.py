05f407d450e84f97f0ea55cbcce67dd7430180be
Aavni-Jon <aavnijon@gmail.com>
2025-01-13 09:21:34 -0800

    Update Blink_Lab_1.py

--- Blink_Lab_1.py
+++ Blink_Lab_1.py
@@ -1,9 +1,34 @@
+# Save on CPX/CPB as code.py
+# Blinks the top right RED LED
 import board
 import digitalio
 import time
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+
+def main():
+    print(dir(board))
+    print("----------------")
+    print(dir(digitalio))
+    print("----------------")
+    print(dir(led))
+    print("----------------")
+    print(dir(digitalio.DigitalInOut))
+    print("----------------")
+    print(dir(led.direction))
+    print("----------------")
+    print(dir(digitalio.Direction))
+    print("----------------")
+    print (dir(led))
+    print("----------------")
+    print(dir(led.value))
+    print(type(led.value))
+    print(dir(board.LED))
+    print(type(board.LED))
+    print("----------------")
+
+main()
 
 while True:
     led.value = True
