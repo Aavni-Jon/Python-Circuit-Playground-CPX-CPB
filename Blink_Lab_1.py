06c93dd94068a167ecb93614e92189c431c8d607
Aavni-Jon <aavnijon@gmail.com>
2025-01-13 09:28:52 -0800

    Update Blink_Lab_1.py

--- Blink_Lab_1.py
+++ Blink_Lab_1.py
@@ -8,30 +8,32 @@ led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 def main():
-    print(dir(board))
+    print(dir(board)) #board library
     print("----------------")
-    print(dir(digitalio))
+    print(dir(digitalio)) #digitalio library
     print("----------------")
-    print(dir(led))
+    print(dir(led)) #variable "led"
     print("----------------")
-    print(dir(digitalio.DigitalInOut))
+    print(dir(digitalio.DigitalInOut)) #Attributes of "led"
     print("----------------")
-    print(dir(led.direction))
+    print(dir(led.direction)) #Attribute of "led.direction"
     print("----------------")
-    print(dir(digitalio.Direction))
+    print(dir(digitalio.Direction)) #Attribute of "digitalio.direction"
+    
+    # How does it blink?
     print("----------------")
     print (dir(led))
     print("----------------")
     print(dir(led.value))
     print(type(led.value))
     print(dir(board.LED))
-    print(type(board.LED))
+    print(type(board.LED)) #LED is the Pin
     print("----------------")
 
 main()
 
 while True:
     led.value = True
-    time.sleep(0.1)
+    time.sleep(0.2) #Line 28 showed that it's a boolean
     led.value = False
-    time.sleep(0.1)
+    time.sleep(0.2)
